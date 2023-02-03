8e9cd27dbb24c674fac69aa37c9140c004225ed4
nesyhadwi <111961915+nesyhadwi@users.noreply.github.com>
2023-02-03 18:17:40 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "For you",
+    introTitle: 'Ahsaaaaaaaaaaan',
+    introDesc: `Kamu itu lucuuu bangett, serius ..
+    aku korban jatuh cinta pandangan pertama sama kamu.
+     aku gabisa ngerangkai kata euy, ya intinya itu deh,
+     maaf ya aku anon mulu, aku malu sama diriku sendiri. ily`,
+    btnIntro: '^^HiHi^^',
+    title: 'Kamu terus tertawa yaaaa',
+    desc: 'aku mabuk ketawanya kamu beuhh, apalagi suara kamu',
+    btnYes: 'aku sukaaaa <33',
+    btnNo: 'ngga:3',
+    question: 'ada lebih dari 7 milliar orang di dunia tapi aku cintanya kamu <3',
+    btnReply: 'buat kamu <3',
+    reply: 'cinta adalah cinta <33333333',
+    mess: 'aku tauu 🥰. cinta kamu 😘😘',
+    messDesc: 'semoga kamu suka sama codingan aku.',
+    btnAccept: 'Okiiiii hunnn <3',
+}
